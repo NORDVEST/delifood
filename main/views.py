e7b2fbed514eb358943241403d76b3c52f5cc29b
Ilyas Gimranov <ilyas.gimranov.2003@mail.ru>
2023-04-23 23:11:14 +0300

    some fix

---
+++ main/views.py
@@ -0,0 +1,69 @@
+from django.forms import model_to_dict
+from django.shortcuts import render, redirect
+from django.http import HttpResponse  # delete if no use
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+from django.views import generic
+
+
+from templatetags.dateshed import addDays
+from .forms import OrdersForm
+from .models import *
+
+
+
+
+# class SignUp(CreateView, generic.ListView):
+#     model = Adress
+#     context_object_name = 'data_adress'  # ваше собственное имя переменной контекста в шаблоне
+#     form_class = SignUpForm
+#     success_url = reverse_lazy("login")  # Перебрасываем на страницу входа в случае успеха
+#     queryset = Adress.objects.all()
+#     template_name = "main/signup.html"
+#
+#
+# def index(request):
+#     data = Orders.objects.all()
+#
+#     return render(request, 'main/index.html', {'data': data})
+
+
+def friends(request):
+    return HttpResponse("<h4>Страница с друзьями</h4>")
+
+
+# def register(request):
+#
+#     if request.POST == 'POST':
+#         form = UserCreationForm(request.POST)  # заполняем его данными из поста
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Вы успешно зарегистрировались')
+#             return redirect('login')  # теперь перебрасываем зарегистрированного на страницу авторизации
+#         else:
+#             messages.error(request, 'Ошибка регистрации')
+#     else:
+#         form = UserCreationForm()
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'main/registration.html', context)
+#
+#
+# def create(request):
+#     error = ''
+#     if request.method == 'POST':
+#         form = OrdersForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#         else:
+#             error = 'Некорректно введены данные'
+#     form = OrdersForm()
+#     data = {
+#         'form': form,
+#         'error': error
+#     }
+#
+#     return render(request, 'main/create.html', data)
