de4e2bc17724b51b8b44104a521653407271dbca
Ilyas Gimranov <ilyas.gimranov.2003@mail.ru>
2023-04-27 21:27:17 +0300

    GoshCode fix)

--- main/models.py
+++ main/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
-    content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    price = models.IntegerField()
-    price_deliver = models.IntegerField()
+    price = models.FloatField()
+    price_deliver = models.FloatField()
     image = models.ImageField()
+    date_deliver = models.DateField(default=timezone.now)
+    time_deliver = models.TimeField(default=timezone.now)
+    cnt_people = models.ImageField(default=2)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
